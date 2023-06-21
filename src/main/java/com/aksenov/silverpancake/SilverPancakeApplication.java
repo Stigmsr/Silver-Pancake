d279918344e1ee44bd890e71375237e6f4619652
Vladislav Aksenov <vlador3et@gmail.com>
2023-06-21 14:59:29 -0400

    Initial Commit

---
+++ src/main/java/com/aksenov/silverpancake/SilverPancakeApplication.java
@@ -0,0 +1,13 @@
+package com.aksenov.silverpancake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilverPancakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilverPancakeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aksenov/silverpancake/SilverPancakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aksenov.silverpancake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilverPancakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
